9d760a43fd2eda85a96f13b06fc695b119649f15
Brad Montgomery <brad@bradmontgomery.net>
2012-02-28 12:30:43 -0600

    updated attributes and template to more cleanly handle master/backup state

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,6 @@
 
 # Which template to use. Including haproxy
 default[:keepalived][:template_suffix] = ""
-
 default[:keepalived][:notification_emails] = []
 default[:keepalived][:notification_email_from] = "root@localhost"
 default[:keepalived][:smtp_server] = nil
@@ -16,12 +15,14 @@ default[:keepalived][:vrrp_instances] = [
   {
     :name               => "VI_1",
     :interface          => "eth0",
-    :state              => "MASTER",
     :nopreempt          => false,     # omitted if false, included if true
-    :advert_int         => nil, # 
+    :advert_int         => nil, # Advertisement Interval (in seconds)
     :virtual_router_id  => 51,
-    :priority           => 101,
+    :master_priority    => 101,     # Priority to use on the Master
+    :backup_priority    => 100,     # Priority to use on the Backup
     :virtual_ipaddress  => ["127.0.0.1"],
+    :backup_nodes       => [], # node names for backup hosts. These will
+                               # be listed with a BACKUP state.
     :track_script      => {
       :name     => "chk_haproxy",
       :script   => "killall -0 haproxy",
